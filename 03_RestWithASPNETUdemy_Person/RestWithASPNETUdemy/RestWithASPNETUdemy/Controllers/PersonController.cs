8d365e8e8f55c045b8e5c40231135d36dcf50f48
Weyne <augustoweyne12@gmail.com>
2021-10-16 16:59:17 -0300

    trabalhando com os 4 verbos (get, put, delete, post)

---
+++ 03_RestWithASPNETUdemy_Person/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult GetSum(string firstNumber, string secondNumber)
+        {
+            return BadRequest();
+        }
+    }
+}
---
+++ 03_RestWithASPNETUdemy_Person/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace RestWithASPNETUdemy.Model
+{
+    public class Person
+    {
+        public long CD_pessoa { get; set; }
+        public string? CH_firstName { get; set; }
+        public string? CH_lastName { get; set; }
+        public string? CH_address { get; set; }
+        public string? CH_gender { get; set; }
+    }
+}
