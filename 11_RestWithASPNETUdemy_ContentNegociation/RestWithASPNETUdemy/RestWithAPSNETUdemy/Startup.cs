6623571d61ebf4ac6f69a6e1e15464193e7c507a
Weyne <augustoweyne12@gmail.com>
2021-10-25 21:00:07 -0300

    Usando Content Negociation para xml e json

--- 11_RestWithASPNETUdemy_ContentNegociation/RestWithASPNETUdemy/RestWithAPSNETUdemy/Startup.cs
+++ 11_RestWithASPNETUdemy_ContentNegociation/RestWithASPNETUdemy/RestWithAPSNETUdemy/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Net.Http.Headers;
 using RestWithAPSNETUdemy.Business;
 using RestWithAPSNETUdemy.Business.Implementations;
 using RestWithAPSNETUdemy.Model.Context;
@@ -45,6 +46,13 @@ namespace RestWithAPSNETUdemy
             {
                 MigrateDatabase(connection);
             }
+
+            services.AddMvc(options =>
+            {
+                options.RespectBrowserAcceptHeader = true;
+                options.FormatterMappings.SetMediaTypeMappingForFormat("xml", MediaTypeHeaderValue.Parse("application/xml"));
+                options.FormatterMappings.SetMediaTypeMappingForFormat("json", MediaTypeHeaderValue.Parse("application/json"));
+            }).AddXmlSerializerFormatters();
 
             //Versionamento da api
             services.AddApiVersioning();
